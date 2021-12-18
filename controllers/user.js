a5f770bafe6fe627dec0f6e40320b6012040f3c9
adramelech <adramelech.psd@gmail.com>
2021-12-18 18:34:55 +0530

    sorting the posts returned after updating profile

--- controllers/user.js
+++ controllers/user.js
@@ -6,85 +6,75 @@ const fs = require("fs");
 const cloudinary = require("cloudinary").v2;
 
 const getUser = async (req, res) => {
-   const { id } = req.params;
-   const user = await User.findOne({ _id: id }).select({ password: 0 });
+    const { id } = req.params;
+    const user = await User.findOne({ _id: id }).select({ password: 0 });
 
-   if (!user) {
-      throw new NotFoundError(`No user exist with id ${id}`);
-   }
+    if (!user) {
+        throw new NotFoundError(`No user exist with id ${id}`);
+    }
 
-   res.status(StatusCodes.OK).json({ user });
+    res.status(StatusCodes.OK).json({ user });
 };
 
 const getUsers = async (req, res) => {
-   const { search } = req.query;
-   if (search) {
-      const regex = new RegExp(search, "i");
-      const user = await User.find({ name: regex }).select({ password: 0 });
-      res.status(StatusCodes.OK).json({ user });
-   } else {
-      const user = await User.find().select({ password: 0 });
-      res.status(StatusCodes.OK).json({ user });
-   }
+    const { search } = req.query;
+    if (search) {
+        const regex = new RegExp(search, "i");
+        const user = await User.find({ name: regex }).select({ password: 0 });
+        res.status(StatusCodes.OK).json({ user });
+    } else {
+        const user = await User.find().select({ password: 0 });
+        res.status(StatusCodes.OK).json({ user });
+    }
 };
 
 const getUsersByIDs = async (req, res) => {
-   const ids = Object.values(req.query);
-   if (!ids) throw new BadRequestError("Expected atleast one id");
-   const user = await User.find({ _id: { $in: ids } }).select({ password: 0 });
-   res.status(StatusCodes.OK).json({ user });
+    const ids = Object.values(req.query);
+    if (!ids) throw new BadRequestError("Expected atleast one id");
+    const user = await User.find({ _id: { $in: ids } }).select({ password: 0 });
+    res.status(StatusCodes.OK).json({ user });
 };
 
 const updateUser = async (req, res) => {
-   const { id } = req.user;
-   const user = await User.findByIdAndUpdate(id, req.body, {
-      new: true,
-      runValidators: true,
-   }).select({ password: 0 });
+    const { id } = req.user;
+    const user = await User.findByIdAndUpdate(id, req.body, {
+        new: true,
+        runValidators: true,
+    }).select({ password: 0 });
 
-   await Post.updateMany(
-      { createdBy: req.user.id },
-      { userDetails: { name: user.name, image: user.profileImage } }
-   );
+    await Post.updateMany({ createdBy: req.user.id }, { userDetails: { name: user.name, image: user.profileImage } });
 
-   const posts = await Post.find({ createdBy: req.user.id });
+    const posts = await Post.find({ createdBy: req.user.id }).sort("-createdAt");
 
-   if (!user) {
-      throw new NotFoundError(`No user exist with id ${id}`);
-   }
-   const token = user.createJWT();
+    if (!user) {
+        throw new NotFoundError(`No user exist with id ${id}`);
+    }
+    const token = user.createJWT();
 
-   res.status(StatusCodes.OK).json({ user, token, posts });
+    res.status(StatusCodes.OK).json({ user, token, posts });
 };
 
 const updateDP = async (req, res) => {
-   const image = req.files?.image;
-   if (!image) {
-      throw new BadRequestError("Expected an image");
-   }
-   const result = await cloudinary.uploader.upload(image.tempFilePath, {
-      use_filename: true,
-      folder: "fb-clone-dps",
-   });
-   fs.unlinkSync(image.tempFilePath);
-   const { secure_url: src } = result;
+    const image = req.files?.image;
+    if (!image) {
+        throw new BadRequestError("Expected an image");
+    }
+    const result = await cloudinary.uploader.upload(image.tempFilePath, {
+        use_filename: true,
+        folder: "fb-clone-dps",
+    });
+    fs.unlinkSync(image.tempFilePath);
+    const { secure_url: src } = result;
 
-   const user = await User.findByIdAndUpdate(
-      req.user.id,
-      { profileImage: src },
-      { new: true, runValidators: true }
-   ).select({ password: 0 });
+    const user = await User.findByIdAndUpdate(req.user.id, { profileImage: src }, { new: true, runValidators: true }).select({ password: 0 });
 
-   await Post.updateMany(
-      { createdBy: req.user.id },
-      { userDetails: { name: user.name, image: user.profileImage } }
-   );
+    await Post.updateMany({ createdBy: req.user.id }, { userDetails: { name: user.name, image: user.profileImage } });
 
-   const posts = await Post.find({ createdBy: req.user.id });
+    const posts = await Post.find({ createdBy: req.user.id });
 
-   if (!user) throw new NotFoundError(`No user exist with id ${req.user.id}`);
+    if (!user) throw new NotFoundError(`No user exist with id ${req.user.id}`);
 
-   res.status(StatusCodes.OK).json({ user, posts });
+    res.status(StatusCodes.OK).json({ user, posts });
 };
 
 module.exports = { getUser, updateUser, updateDP, getUsers, getUsersByIDs };

2e309b5b70950fdcf9825370c656f4d3f1674d1c
adarsh <adramelech.psd@gmail.com>
2022-06-16 18:16:52 +0530

    change incorrect password response

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,47 +1,51 @@
 const User = require("../models/User");
 const { StatusCodes } = require("http-status-codes");
-const { BadRequestError, NotFoundError, AuthenticationError } = require("../errors");
+const {
+	BadRequestError,
+	NotFoundError,
+	AuthenticationError,
+} = require("../errors");
 
 const register = async (req, res) => {
-   let user = await User.findOne({ email: req.body.email });
-   if (user) throw new BadRequestError("User already exists");
-   user = await User.create({ ...req.body });
-   const { _id: id, name, profileImage } = user;
-   const token = user.createJWT();
-   res.status(StatusCodes.CREATED).json({
-      id,
-      token,
-      name,
-      profileImage,
-   });
+	let user = await User.findOne({ email: req.body.email });
+	if (user) throw new BadRequestError("User already exists");
+	user = await User.create({ ...req.body });
+	const { _id: id, name, profileImage } = user;
+	const token = user.createJWT();
+	res.status(StatusCodes.CREATED).json({
+		id,
+		token,
+		name,
+		profileImage,
+	});
 };
 
 const login = async (req, res) => {
-   if (!req.body.email || !req.body.password) {
-      throw new BadRequestError("Please provide email and password");
-   }
+	if (!req.body.email || !req.body.password) {
+		throw new BadRequestError("Please provide email and password");
+	}
 
-   const user = await User.findOne({ email: req.body.email });
+	const user = await User.findOne({ email: req.body.email });
 
-   if (!user) {
-      throw new NotFoundError("Invalid credentials");
-   }
+	if (!user) {
+		throw new NotFoundError("Invalid credentials");
+	}
 
-   const isPasswordCorrect = await user.comparePassword(req.body.password);
+	const isPasswordCorrect = await user.comparePassword(req.body.password);
 
-   if (!isPasswordCorrect) {
-      throw new AuthenticationError("Invalid credentials");
-   }
+	if (!isPasswordCorrect) {
+		throw new AuthenticationError("It's Ezio's password!! Enter yours");
+	}
 
-   const { _id: id, name, profileImage } = user;
-   const token = user.createJWT();
+	const { _id: id, name, profileImage } = user;
+	const token = user.createJWT();
 
-   res.status(StatusCodes.OK).json({
-      id,
-      token,
-      name,
-      profileImage,
-   });
+	res.status(StatusCodes.OK).json({
+		id,
+		token,
+		name,
+		profileImage,
+	});
 };
 
 module.exports = { register, login };

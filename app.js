c6c3c3135c8fd4d96ecb1d0876efb6f0b355f5e9
adarsh <adramelech.psd@gmail.com>
2022-06-19 10:15:16 +0530

    change allowed headers

--- app.js
+++ app.js
@@ -1,26 +1,26 @@
 //dependencies
 
-require('dotenv').config();
-require('express-async-errors');
-const fileUpload = require('express-fileupload');
-const express = require('express');
-const cloudinary = require('cloudinary').v2;
-const connectDB = require('./db/connect');
+require("dotenv").config();
+require("express-async-errors");
+const fileUpload = require("express-fileupload");
+const express = require("express");
+const cloudinary = require("cloudinary").v2;
+const connectDB = require("./db/connect");
 
 //security dependencies
 
-const helmet = require('helmet');
-const cors = require('cors');
-const xss = require('xss-clean');
+const helmet = require("helmet");
+const cors = require("cors");
+const xss = require("xss-clean");
 
 //app initialisation
 
 const app = express();
-const server = require('http').createServer(app);
-const { Server } = require('socket.io');
+const server = require("http").createServer(app);
+const { Server } = require("socket.io");
 const io = new Server(server, {
 	cors: {
-		origin: 'https://adramelech-social-media-app.netlify.app',
+		origin: "https://adramelech-social-media-app.netlify.app",
 	},
 });
 const PORT = process.env.PORT || 5000;
@@ -35,50 +35,58 @@ cloudinary.config({
 
 //Routes
 
-const authRouter = require('./routes/auth');
-const userRouter = require('./routes/user');
-const postRouter = require('./routes/post');
-const chatRouter = require('./routes/chat');
-const messageRouter = require('./routes/message');
+const authRouter = require("./routes/auth");
+const userRouter = require("./routes/user");
+const postRouter = require("./routes/post");
+const chatRouter = require("./routes/chat");
+const messageRouter = require("./routes/message");
 
 //middlewares
 
-const errorHandlerMiddleware = require('./middleware/error-handler');
-const authorizationMiddleware = require('./middleware/authorization');
-const notFoundMiddleware = require('./middleware/not-found');
+const errorHandlerMiddleware = require("./middleware/error-handler");
+const authorizationMiddleware = require("./middleware/authorization");
+const notFoundMiddleware = require("./middleware/not-found");
 
 app.use(xss());
 app.use(helmet());
 app.use(express.json());
 app.use(fileUpload({ useTempFiles: true }));
-app.use(cors({origin: 'https://adramelech-social-media-app.netlify.app',allowedHeaders:"Content-Type,Authorization,withCredentials",}));
+app.use(cors({ origin: "https://adramelech-social-media-app.netlify.app" }));
 
-app.get('/', (req, res) => {
-	res.status(200).json({ msg: 'welcome' });
+app.get("/", (req, res) => {
+	res.status(200).json({ msg: "welcome" });
 });
 
 // socket io
 
-const { addUser, getUserID, getSocketID, removeUser, createMessage } = require('./socket/users');
+const {
+	addUser,
+	getUserID,
+	getSocketID,
+	removeUser,
+	createMessage,
+} = require("./socket/users");
 
-io.on('connection', socket => {
-	io.emit('usersOnline', addUser(socket.handshake.query.id, socket.id));
-	socket.on('send message', async (message, to, chatID, id) => {
+io.on("connection", socket => {
+	io.emit("usersOnline", addUser(socket.handshake.query.id, socket.id));
+	socket.on("send message", async (message, to, chatID, id) => {
 		await createMessage(chatID, id, message);
-		socket.to(getSocketID(to)).emit('receive message', message, getUserID(socket.id));
+		socket
+			.to(getSocketID(to))
+			.emit("receive message", message, getUserID(socket.id));
 	});
-	socket.on('disconnect', () => {
-		io.emit('usersOnline', removeUser(socket.id));
+	socket.on("disconnect", () => {
+		io.emit("usersOnline", removeUser(socket.id));
 	});
 });
 
 //routes
 
-app.use('/api/v1/auth', authRouter);
-app.use('/api/v1/user', authorizationMiddleware, userRouter);
-app.use('/api/v1/post', authorizationMiddleware, postRouter);
-app.use('/api/v1/chat', authorizationMiddleware, chatRouter);
-app.use('/api/v1/message', authorizationMiddleware, messageRouter);
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/user", authorizationMiddleware, userRouter);
+app.use("/api/v1/post", authorizationMiddleware, postRouter);
+app.use("/api/v1/chat", authorizationMiddleware, chatRouter);
+app.use("/api/v1/message", authorizationMiddleware, messageRouter);
 
 app.use(errorHandlerMiddleware);
 app.use(notFoundMiddleware);

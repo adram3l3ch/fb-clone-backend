3406b49e321b075cb7337e7eb33fc66708be44fd
adarsh <adramelech.psd@gmail.com>
2022-06-26 21:00:18 +0530

    add profile image in req.user

--- middleware/authorization.js
+++ middleware/authorization.js
@@ -2,19 +2,19 @@ const { AuthenticationError } = require("../errors");
 const jwt = require("jsonwebtoken");
 
 const authorize = (req, res, next) => {
-   const authHeader = req.headers.authorization;
-   if (!authHeader || !authHeader.startsWith("Bearer")) {
-      throw new AuthenticationError("Authentication Invalid");
-   }
-   const token = authHeader.split(" ")[1];
+	const authHeader = req.headers.authorization;
+	if (!authHeader || !authHeader.startsWith("Bearer")) {
+		throw new AuthenticationError("Authentication Invalid");
+	}
+	const token = authHeader.split(" ")[1];
 
-   try {
-      const payload = jwt.verify(token, process.env.JWT_SECRET);
-      req.user = { id: payload.id, name: payload.name };
-      next();
-   } catch (error) {
-      throw new AuthenticationError("Authentication Invalid");
-   }
+	try {
+		const payload = jwt.verify(token, process.env.JWT_SECRET);
+		req.user = { id: payload.id, name: payload.name, profileImage: payload.profileImage };
+		next();
+	} catch (error) {
+		throw new AuthenticationError("Authentication Invalid");
+	}
 };
 
 module.exports = authorize;
